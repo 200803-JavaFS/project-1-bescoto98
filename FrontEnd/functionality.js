9df129f7f443ea646c8ab465adf25d1ef253a3c9
bescoto98 <escoto.brenda42@gmail.com>
2020-09-06 13:47:11 -0700

    more front end work

--- FrontEnd/functionality.js
+++ FrontEnd/functionality.js
@@ -1,7 +1,49 @@
 const url = "http://localhost:8080/project1/"
 
-// let userdto = null;
+function translateStatus(s){
+  if(s == 1){
+    return "PENDING";
+  }
+  if(s == 2){
+    return "APPROVED";
+  }
+  if(s == 3){
+    return "DENIED";
+  }
+}
 
+function showPending(){
+  let status = document.getElementById("pendingbool");
+  let tbl = document.getElementById("allReimb");
+  let rows = tbl.rows;
+
+  if(status.checked){
+      for(let i = 1; i < rows.length; i++){
+        if(rows[i].getElementsByTagName("td")[5].innerText.includes("APPROVED")
+        || rows[i].getElementsByTagName("td")[5].innerText.includes("DENIED")){
+        rows[i].style.display = "none";
+      }
+    }
+  }
+  else {
+    for(let i = 1; i < rows.length; i++){
+      rows[i].style.display = "table-row";
+    }
+
+  }
+
+}
+
+function showApproved(){
+
+}
+
+function showDenied(){
+
+}
+
+
+// function to kick off login services
 async function loginfunc(){
 
   let u = document.getElementById("username").value;
@@ -35,6 +77,7 @@ async function loginfunc(){
   }
 }
 
+// will return an object with most important user information
 async function getUserDTO(){
   let getCred = await fetch(url + "login",
   {
@@ -49,6 +92,7 @@ async function getUserDTO(){
   }
 }
 
+//return all reimbursement tickets
 async function getAll(){
 
   let resp = await fetch(url + "reimbursement",{
@@ -58,8 +102,38 @@ async function getAll(){
   if(resp.status === 200){
     let data = await resp.json();
     console.log(data);
+
+    let btn = document.getElementById("showAllBtn");
+    btn.style.display = "none";
+
+    let index = 1;
+
+    let tbl = document.getElementById("allReimb");
+
+    for(let reimbursement of data){
+
+      let row = tbl.insertRow(index);
+      let c0 = row.insertCell(0);
+      let c1 = row.insertCell(1);
+      let c2 = row.insertCell(2);
+      let c3 = row.insertCell(3);
+      let c4 = row.insertCell(4);
+      let c5 = row.insertCell(5);
+
+      c0.innerHTML = reimbursement.r_id;
+      c1.innerHTML = reimbursement.r_amnt;
+      c2.innerHTML = reimbursement.r_submitted;
+      if(reimbursement.r_resolved != null){
+          c3.innerHTML = reimbursement.r_resolved;
+      }
+      c4.innerHTML = reimbursement.r_author;
+      c5.innerHTML = translateStatus(reimbursement.r_status);
+
+      index++;
+    }
   }
 }
+
 
 async function getUsersName(){
   console.log("getting name");
